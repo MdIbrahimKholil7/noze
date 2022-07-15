3180592bc0e85139ca87b18f82c4056fa88ae253
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-07-15 13:54:36 +0600

    banner complete

--- src/components/HomeComponents/Banner.js
+++ src/components/HomeComponents/Banner.js
@@ -4,17 +4,17 @@ const Banner = () => {
     return (
         <div>
             <div>
-                <div class="hero min-h-screen bg-base-100">
+                <div class="hero h-[50vh] bg-base-100">
                     <div class="hero-content gap-32 flex-col lg:flex-row w-full justify-between max-w-7xl mx-auto lg:px-20">
 
-                        <div>
+                        <div className='text-center lg:text-left mt-16 lg:mt-0'>
                             <h1 class="text-5xl font-bold">There’s something</h1>
                             <h1 class="text-5xl font-bold">new in the air</h1>
-                            <p class="py-6">NOZE is an AI powered air quality monitor that works to keep you safe. Breathe easy.</p>
-                            <button class="btn btn-primary">Join the waitlist</button>
+                            <p class="py-6 text-[18px]">NOZE is an AI powered air quality monitor that works to keep you safe. Breathe easy.</p>
+                            <button class="btn bg-[#654a9b] text-white">Join the waitlist</button>
                         </div>
                         <div>
-                            <img  src={img} class="max-w-[400px] rounded-lg shadow-2xl" alt='nozHeroImg' />
+                            <img  src={img} class="max-w-[400px] h-[550px] rounded-lg shadow-2xl" alt='nozHeroImg' />
                         </div>
                     </div>
                 </div>
