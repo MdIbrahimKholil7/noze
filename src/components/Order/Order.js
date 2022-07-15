fd465c43d3ea95cdef3561dd0b66defc5aee2fcb
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-07-15 11:25:52 +0600

    header border add

---
+++ src/components/Order/Order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Order = () => {
+    return (
+        <div>
+            <div className='flex gap-9 items-center '>
+                <h1 className='text-[17px] text-[#6b5a78]'>Order for $249</h1>
+                <button className='bg-[#654a9b] font-bold rounded-md px-[30px] py-[10px]  text-white'>Pre Order</button>
+            </div>
+        </div>
+    );
+};
+
+export default Order;
