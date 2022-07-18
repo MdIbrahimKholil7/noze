f74e3bd66fc2c18aa8edd3aa1790d88f562d70fb
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-07-18 12:41:55 +0600

    conqure section complete

---
+++ src/components/Home/Conquere/Conquere.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from 'react';
+import video from '../../../assets/video-5.mp4'
+const Conquere = () => {
+
+    const videoRef=useRef(null)
+    console.log(videoRef.current)
+    useEffect(()=>{
+        videoRef.current.play()
+    },[])
+    return (
+        <div>
+            <div class="hero min-h-screen bg-base-100">
+                <div class="hero-content justify-between w-full  p-5 flex-col lg:flex-row-reverse gap-4">
+                    <div class="text-center lg:text-left lg:w-[550px]">
+                        <h1 class="text-5xl font-[500]">Сonquer the Virus</h1>
+                        <p class="py-6">Сonquer the Virus
+                            Noze tracks the airborne markers (VOCs) associated with the Coronavirus family. Our AI uses that information to deliver a real-time risk index that will not only tell you if your space is prone to spreading the virus, but whether there’s a chance the virus is actually in the air.</p>
+                    </div>
+                    <div>
+                        {/*  <video src="https://getnoze.com/wp-content/themes/twentytwentyone/noze/assets/movies/NOZEabout1.mp4" autoPlay loop muted='muted' playsInline controlsList='nodownload'></video> */}
+                        {/*  <video width="320" height="240" autoplay>
+                            <source src={video}/>
+                            Your browser does not support the video tag.
+                        </video> */}
+                       <video ref={videoRef} class="lg:w-[454px]" src={video} autoplay loop muted="muted" playsinline="" controlslist="nodownload" ></video>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Conquere;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Banner from '../HomeComponents/Banner';
 import Headline from '../HomeComponents/Headline';
+import Conquere from './Conquere/Conquere';
 
 const Home = () => {
     return (
         <div className='max-w-7xl mx-auto'>
             <Banner/>
             <Headline/>
-           
+           <Conquere/>
         </div>
     );
 };
